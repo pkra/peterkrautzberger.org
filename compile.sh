2be9697f9fc4900da285eac705e5cca1785ccac9
Peter Krautzberger <p.krautzberger@gmail.com>
2018-10-27 23:35:07 +0200

    remove jekyll, travis

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e # halt script on error
-echo "building jekyll"
-bundle exec jekyll build --destination out
-for i in `find out -name '*.html'` ; do
-  echo "running mathjax-node on $i"
-  node node_modules/mathjax-node-page/bin/mjpage --dollars < "$i" | sponge "$i"
-done
