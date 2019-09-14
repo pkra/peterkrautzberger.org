7504be7f4b6de4af40e9d6fdb96e1f37a7e6d582
Peter Krautzberger <p.krautzberger@gmail.com>
2019-09-14 21:38:43 +0200

    fix: update to latest PWA recs

--- .eleventy.js
+++ .eleventy.js
@@ -33,6 +33,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("public");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("serviceworker.js");
 
