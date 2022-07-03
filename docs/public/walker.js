d04fa1ee21d8a2f556d53be0a935e3a5fc91ab27
Peter Krautzberger <p.krautzberger@gmail.com>
2022-07-03 21:57:31 +0200

    chore: regenerate site

--- docs/public/walker.js
+++ docs/public/walker.js
@@ -1,2 +1,2 @@
-    import { attachNavigator } from 'https://cdn.skypack.dev/@krautzource/aria-tree-walker@^3.1.1';
+    import { attachNavigator } from 'https://cdn.skypack.dev/@krautzource/aria-tree-walker@3.3.0';
     [...document.querySelectorAll('[role="tree"]')].filter( node => node.hasAttribute('data-owns')).forEach(attachNavigator);
